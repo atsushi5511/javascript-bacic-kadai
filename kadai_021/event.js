a1135f9a810753e033fecd3eac0f5f49e650e1f3
atsushi5511 <143787064+atsushi5511@users.noreply.github.com>
2023-12-09 08:03:28 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click',() => {
+    setTimeout(() => {
+    document.getElementById('text').textContent = 'ボタンをクリックしました';
+}, 2000);
+});
+
